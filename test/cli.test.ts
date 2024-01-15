80f9ea3cf9e497ebdef71ace13e9b34a3fa99811
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-01-15 16:51:33 -0700

    expand tests

--- test/cli.test.ts
+++ test/cli.test.ts
@@ -3,39 +3,87 @@
 import { afterEach, beforeEach, describe, it } from "deno_std/testing/bdd.ts";
 import { expect, mock } from "./mocked.ts";
 
+import { ValidationError } from "cliffy/command/mod.ts";
 import { _internals, global } from "../src/cli.ts";
 
 describe("cli", () => {
-  // deno-lint-ignore no-explicit-any
-  let cli: any;
-  let stubConsoleLog: mock.Spy;
-  let stubConsoleError: mock.Spy;
-  let spyGetEnv: mock.Spy;
+  describe("global()", () => {
+    let spyGetEnv: mock.Spy;
 
-  beforeEach(() => {
-    stubConsoleError = mock.stub(console, "error");
-    stubConsoleLog = mock.stub(console, "log");
-    spyGetEnv = mock.spy(_internals, "getEnv");
+    function createCLI() {
+      return global()
+        .noExit()
+        .throwErrors();
+    }
 
-    cli = global()
-      .noExit()
-      .throwErrors();
-  });
+    beforeEach(() => {
+      spyGetEnv = mock.spy(_internals, "getEnv");
+    });
 
-  afterEach(() => {
-    stubConsoleError.restore();
-    stubConsoleLog.restore();
-    spyGetEnv.restore();
-  });
+    afterEach(() => {
+      spyGetEnv.restore();
+    });
 
-  it("returns on --help", async () => {
-    const stubShowHelp = mock.stub(cli, "showHelp");
-    const result = await cli.parse(["--help"]);
+    it("returns on --help", async () => {
+      const cli = createCLI();
+      const stubShowHelp = mock.stub(cli, "showHelp");
+      const result = await cli.parse(["--help"]);
 
-    expect(result).to.exist();
-    expect(stubShowHelp).to.have.been.called(1);
-    expect(spyGetEnv).to.have.been.deep.calledWith([
-      "DEPLOYER_IDENTITY_DIR",
-    ]);
+      expect(result.options).to.deep.equal({
+        help: true,
+        identityDir: Deno.cwd(),
+      });
+
+      expect(stubShowHelp).to.have.been.called(1);
+      expect(spyGetEnv).to.have.been.deep.calledWith([
+        "DEPLOYER_IDENTITY_DIR",
+      ]);
+    });
+    it("returns expected options with defaults", async () => {
+      const result = await createCLI().parse(["--env", "local"]);
+
+      expect(result.options).to.deep.equal({
+        env: "local",
+        identityDir: Deno.cwd(),
+      });
+      expect(spyGetEnv).to.have.been.deep.calledWith([
+        "DEPLOYER_IDENTITY_DIR",
+      ]);
+    });
+    it("returns expected options with explicit -I", async () => {
+      const result = await createCLI().parse([
+        "--env",
+        "local",
+        "--identity-dir",
+        "/devel/infra/root",
+      ]);
+
+      expect(result.options).to.deep.equal({
+        env: "local",
+        identityDir: "/devel/infra/root",
+      });
+    });
+    it("returns expected options with env-var", async () => {
+      spyGetEnv.restore();
+      spyGetEnv = mock.stub(
+        _internals,
+        "getEnv",
+        (_: string) => ("/from-env/path"),
+      );
+      const result = await createCLI().parse([
+        "--env",
+        "local",
+      ]);
+
+      expect(result.options).to.deep.equal({
+        env: "local",
+        identityDir: "/from-env/path",
+      });
+    });
+
+    it("throws on missing --env", async () => {
+      const cli = createCLI();
+      await expect(cli.parse([])).to.be.rejectedWith(ValidationError);
+    });
   });
 });
