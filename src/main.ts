eb6efd6aab432306548d3fed2b9cc24c73ad9ec0
Matthew A. Miller <linuxwolf@outer-planes.net>
2024-01-16 20:40:52 -0700

    feat: decrypt file (hidden)

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@
 import { Command } from "cliffy/command/mod.ts";
 import { globalCommand } from "./internal/global.ts";
 import { encryptCommand } from "./cmd/encrypt.ts";
+import { decryptCommand } from "./cmd/decrypt.ts";
 
 export function createCommand() {
   let cli: Command = globalCommand();
   cli = encryptCommand(cli);
+  cli = decryptCommand(cli);
 
   return cli;
 }
---
+++ test/cmd/decrypt.test.ts
@@ -0,0 +1,79 @@
+/** */
+
+import { afterEach, beforeEach, describe, it } from "deno_std/testing/bdd.ts";
+import { expect, mock } from "../mocked.ts";
+
+import { Command } from "cliffy/command/mod.ts";
+
+import { globalCommand, GlobalConfig } from "../../src/internal/global.ts";
+import { _internals, decryptCommand } from "../../src/cmd/decrypt.ts";
+
+describe("cmd/decrypt", () => {
+  // deno-lint-ignore no-explicit-any
+  let global: any & Command;
+
+  beforeEach(() => {
+    global = globalCommand();
+  });
+
+  describe("encryptCommand", () => {
+    let spyCreateKeyOp: mock.Spy | undefined;
+    let spyDecrypt: mock.Spy | undefined;
+
+    beforeEach(() => {
+      const createKeyOp = _internals.createKeyOp;
+      spyCreateKeyOp = mock.stub(
+        _internals,
+        "createKeyOp",
+        (config: GlobalConfig) => {
+          const op = createKeyOp(config);
+          spyDecrypt = mock.stub(op, "decrypt");
+          return op;
+        },
+      );
+    });
+
+    afterEach(() => {
+      spyCreateKeyOp && !spyCreateKeyOp.restored && spyCreateKeyOp.restore();
+    });
+
+    it("creates with expected parameters", () => {
+      const result: Command = decryptCommand(global);
+
+      const decrypt = result.getCommand("decrypt", true);
+      expect(decrypt).to.exist();
+      expect(decrypt?.getName()).to.equal("decrypt");
+      expect(decrypt?.getArgsDefinition()).to.equal("<file:file>");
+
+      let opt;
+
+      opt = decrypt?.getGlobalOption("env");
+      expect(opt).to.exist();
+
+      opt = decrypt?.getGlobalOption("identity-dir");
+      expect(opt).to.exist();
+
+      expect(result.getCommand("decyrpt")).to.be.undefined();
+    });
+
+    it("calls the handler on parse", async () => {
+      const cmd = decryptCommand(global);
+      await cmd.parse([
+        "decrypt",
+        "--env",
+        "testing",
+        "secrets.env",
+      ]);
+
+      expect(spyCreateKeyOp).to.have.been.deep.calledWith([
+        {
+          env: "testing",
+          identityDir: Deno.cwd(),
+        },
+      ]);
+      expect(spyDecrypt).to.have.been.deep.calledWith([
+        "secrets.env",
+      ]);
+    });
+  });
+});
